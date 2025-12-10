4e175d32f17124c61e8386e99df23d206ee7b913
jnicros09 <jnicros@gmail.com>
2025-12-10 15:06:18 +0800

    Add email entry for transaction 21

--- database/seed/data_changes.sql
+++ database/seed/data_changes.sql
@@ -3,3 +3,6 @@ VALUES ('Jonna', 'Fresado', '09550957912', 'jonnaf07@gmail.com');
 
 INSERT INTO transactions (user_id, amount, mode_of_payment, transaction_number, reference_number)
 VALUES (21, 500, 'Maya', 'TXN0005783421', 'ECO-111532462');
+
+INSERT INTO email (user_id, transaction_id, sent_at, status)
+VALUES (21, 21, NOW(), 'sent');
