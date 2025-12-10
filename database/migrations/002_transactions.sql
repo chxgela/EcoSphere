a76c86fd0ef48f8335cb8b473c0375071f5d9b3a
jnicros09 <jnicros@gmail.com>
2025-12-10 14:38:17 +0800

    Add migration 002_transactions.sql for transactions table

---
+++ database/migrations/002_transactions.sql
@@ -0,0 +1,10 @@
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    mode_of_payment VARCHAR(50) NOT NULL,
+    transaction_number VARCHAR(50) UNIQUE NOT NULL,
+    reference_number VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
