ce188c4d579fd324f8c795e52046be9160465c57
chxgela <gelaandal05@gmail.com>
2025-12-10 15:07:18 +0800

    Update user 1 contact

--- database/seed/data_changes.sql
+++ database/seed/data_changes.sql
@@ -6,3 +6,7 @@ VALUES (21, 500, 'Maya', 'TXN0005783421', 'ECO-111532462');
 
 INSERT INTO email (user_id, transaction_id, sent_at, status)
 VALUES (21, 21, NOW(), 'sent');
+
+UPDATE users 
+SET contact = '09922346892'
+WHERE id = 1;
