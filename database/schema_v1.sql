14763e31e3a0d3acbff8514369eb725574b3ec07
chxgela <gelaandal05@gmail.com>
2025-12-10 14:31:08 +0800

    Add transactions table to schema_v1

--- database/schema_v1.sql
+++ database/schema_v1.sql
@@ -5,3 +5,14 @@ CREATE TABLE users (
     contact VARCHAR(20) UNIQUE NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL
 );
+
+CREATE TABLE transactions (
+    id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    amount NUMERIC(10, 2) NOT NULL,
+    mode_of_payment VARCHAR(50) NOT NULL,
+    transaction_number VARCHAR(50) UNIQUE NOT NULL,
+    reference_number VARCHAR(50) UNIQUE NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+);
