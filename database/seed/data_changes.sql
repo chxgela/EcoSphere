9ba88d20ae4f57b3cb553e49144849c60e83d746
chxgela <gelaandal05@gmail.com>
2025-12-10 15:05:26 +0800

    Add transaction for user 21

--- database/seed/data_changes.sql
+++ database/seed/data_changes.sql
@@ -1,2 +1,5 @@
 INSERT INTO users (firstname, lastname, contact, email)
 VALUES ('Jonna', 'Fresado', '09550957912', 'jonnaf07@gmail.com');
+
+INSERT INTO transactions (user_id, amount, mode_of_payment, transaction_number, reference_number)
+VALUES (21, 500, 'Maya', 'TXN0005783421', 'ECO-111532462');
